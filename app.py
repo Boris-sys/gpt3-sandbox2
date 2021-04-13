fd05074c87b7b0783abf1cecbfcaa1970cd354be
Boris <borissicsac@gmail.com>
2021-04-13 14:22:45 -0300

    Ejemplos... de nuevo...

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ title = '¿Que le respondo al profe?'
 context = 'El objetivo de esta simulación es encontrar la respuesta perfecta para darle al profe en una situación complicada.'
 
 examples={
-'Profesor: que pensás hacer respecto a tus trabajos prácticos?':'Yo: Trataré de hacer todos los trabajos prácticos antes de que terminen las clases.',
-'Profesor: Alguna duda te quedó respecto de la clase?':'Yo: Creo que me gustaría que volviera a explicar el tema pero con menos detalles para tener una idea más clara.',
-'Profesor: Todo listo para el examen final?':'Yo:  Si profe, he estado estudiando todos los temas, alguna recomendación sobre qué estudiar en específico?',
-'Profesor: Has estado teniendo algún problema al llevar la clase?':'Yo:  En general todo bien, pero he tenido algunos problemas con un par de temas.'
+"Profesor: que pensás hacer respecto a tus trabajos prácticos?":"Yo: Trataré de hacer todos los trabajos prácticos antes de que terminen las clases.",
+"Profesor: Alguna duda te quedó respecto de la clase?":"Yo: Creo que me gustaría que volviera a explicar el tema pero con menos detalles para tener una idea más clara.",
+"Profesor: Todo listo para el examen final?":"Yo:  Si profe, he estado estudiando todos los temas, alguna recomendación sobre qué estudiar en específico?",
+"Profesor: Has estado teniendo algún problema al llevar la clase?":"Yo:  En general todo bien, pero he tenido algunos problemas con un par de temas."
 }
 
 gpt = GPT(engine="davinci",
@@ -34,6 +34,7 @@ gpt = GPT(engine="davinci",
           context=context)
 
 gpt.add_examples(examples)
+
 
 
 if __name__ == "__main__":
